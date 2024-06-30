d3c23ba874e7b1ca16bd0a16560d7d61d514d471
Sr.Caveira <68562204+SirCaveiraOFC@users.noreply.github.com>
2024-06-30 03:07:53 -0300

    Create getSteamWebAPIToken.js

---
+++ getSteamWebAPIToken.js
@@ -0,0 +1,23 @@
+(async function() {
+    const current_token = await getWebapiToken();
+
+    prompt("Token atual:", current_token);
+})();
+
+async function getWebapiToken() {
+    let webapi_token = null;
+
+    if (window.application_config?.dataset?.loyalty_webapi_token) {
+        webapi_token = JSON.parse(window.application_config.dataset.loyalty_webapi_token);
+    } else {
+        const res = await fetch('/category/action');
+        const html = await res.text();
+        const doc = new DOMParser().parseFromString(html, 'text/html');
+        const token = doc.getElementById('application_config')?.dataset?.loyalty_webapi_token;
+        if (!token) {
+            throw new Error('No valid API token found. Are you logged in?');
+        }
+        webapi_token = JSON.parse(token);
+    }
+    return webapi_token;
+}
